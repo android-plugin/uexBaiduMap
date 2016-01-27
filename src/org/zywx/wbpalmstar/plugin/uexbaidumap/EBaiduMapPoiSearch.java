63e02a7faa9dc643951d6bd9ee5ea366929582c0
BadWaka <1456683844@qq.com>
2016-01-27 14:21:34 +0800

    增加了，当不打开地图View时，也可以调用poi搜索功能（目前支持城市检索，周边检索，区域检索）

--- src/org/zywx/wbpalmstar/plugin/uexbaidumap/EBaiduMapPoiSearch.java
+++ src/org/zywx/wbpalmstar/plugin/uexbaidumap/EBaiduMapPoiSearch.java
@@ -30,20 +30,35 @@ import com.baidu.mapapi.search.sug.SuggestionResult;
 import com.baidu.mapapi.search.sug.SuggestionSearch;
 import com.baidu.mapapi.search.sug.SuggestionSearchOption;
 
-public class EBaiduMapPoiSearch implements OnGetPoiSearchResultListener,
-		OnGetSuggestionResultListener {
+public class EBaiduMapPoiSearch implements OnGetPoiSearchResultListener, OnGetSuggestionResultListener {
 	private String TAG = "EBaiduMapPoiSearch";
 	protected Context mContext;
-	protected BaiduMap mBaiduMap;
-	protected MapView mMapView;
+	protected BaiduMap mBaiduMap = null;
+	protected MapView mMapView = null;
 	private PoiSearch mPoiSearch = null;
 	private SuggestionSearch mSuggestionSearch = null;
-    private EBaiduMapBaseFragment baseFragment;
+	private EBaiduMapBaseFragment baseFragment = null;
 
-	public EBaiduMapPoiSearch(EBaiduMapBaseFragment context, BaiduMap baiduMap,
-			MapView mapView) {
+	private EBaiduMapBaseNoMapViewManager mEBaiduMapBaseNoMapViewManager;
+
+	/**
+	 * 不开启MapView的构造方法 
+	 * 
+	 * @param mapBaseNoMapViewManager
+	 */
+	public EBaiduMapPoiSearch(EBaiduMapBaseNoMapViewManager mapBaseNoMapViewManager) {
+		mContext = mapBaseNoMapViewManager.getmContext();
+		mEBaiduMapBaseNoMapViewManager = mapBaseNoMapViewManager;
+		// 初始化搜索模块，注册搜索事件监听
+		mPoiSearch = PoiSearch.newInstance();
+		mPoiSearch.setOnGetPoiSearchResultListener(this);
+		mSuggestionSearch = SuggestionSearch.newInstance();
+		mSuggestionSearch.setOnGetSuggestionResultListener(this);
+	}
+
+	public EBaiduMapPoiSearch(EBaiduMapBaseFragment context, BaiduMap baiduMap, MapView mapView) {
 		mContext = context.getActivity();
-        baseFragment = context;
+		baseFragment = context;
 		mBaiduMap = baiduMap;
 		mMapView = mapView;
 		// 初始化搜索模块，注册搜索事件监听
@@ -55,51 +70,45 @@ public class EBaiduMapPoiSearch implements OnGetPoiSearchResultListener,
 
 	public void poiSearchInCity(String city, String searchKey, int pageNum) {
 		Log.i(TAG, "poiSearchInCity");
-		mPoiSearch.searchInCity((new PoiCitySearchOption()).city(city)
-				.keyword(searchKey).pageNum(pageNum));
+		mPoiSearch.searchInCity((new PoiCitySearchOption()).city(city).keyword(searchKey).pageNum(pageNum));
 	}
 
-	public void poiNearbySearch(double lng, double lat, int radius,
-			String searchKey, int pageNum) {
+	public void poiNearbySearch(double lng, double lat, int radius, String searchKey, int pageNum) {
 		Log.i(TAG, "poiNearbySearch");
 		LatLng ll = new LatLng(lat, lng);
-		mPoiSearch.searchNearby((new PoiNearbySearchOption()).location(ll)
-				.radius(radius).keyword(searchKey).pageNum(pageNum));
+		mPoiSearch.searchNearby(
+				(new PoiNearbySearchOption()).location(ll).radius(radius).keyword(searchKey).pageNum(pageNum));
 	}
 
-	public void poiBoundSearch(double east, double north, double west,
-			double south, String searchKey, int pageNum) {
+	public void poiBoundSearch(double east, double north, double west, double south, String searchKey, int pageNum) {
 		Log.i(TAG, "poiBoundSearch");
 		LatLng northeast = new LatLng(north, east);
 		LatLng southwest = new LatLng(south, west);
-		LatLngBounds bounds = new LatLngBounds.Builder().include(northeast)
-				.include(southwest).build();
-		mPoiSearch.searchInBound((new PoiBoundSearchOption()).bound(bounds)
-				.keyword(searchKey).pageNum(pageNum));
+		LatLngBounds bounds = new LatLngBounds.Builder().include(northeast).include(southwest).build();
+		mPoiSearch.searchInBound((new PoiBoundSearchOption()).bound(bounds).keyword(searchKey).pageNum(pageNum));
 	}
 
 	/**
 	 * 使用建议搜索服务获取建议列表，结果在onGetSuggestionResult()中更新
 	 */
-	public void requestSuggestion(String city, String key, double lat,
-			double lng) {
+	public void requestSuggestion(String city, String key, double lat, double lng) {
 		Log.i(TAG, "requestSuggestion");
 		LatLng ll = new LatLng(lat, lng);
-		mSuggestionSearch.requestSuggestion((new SuggestionSearchOption())
-				.keyword(key).city(city).location(ll));
+		mSuggestionSearch.requestSuggestion((new SuggestionSearchOption()).keyword(key).city(city).location(ll));
 	}
 
 	@Override
 	public void onGetPoiResult(PoiResult result) {
-		if (result == null
-				|| result.error == SearchResult.ERRORNO.RESULT_NOT_FOUND) {
+		if (result == null || result.error == SearchResult.ERRORNO.RESULT_NOT_FOUND) {
 			jsonNoResultCallback();
 			return;
 		}
 		if (result.error == SearchResult.ERRORNO.NO_ERROR) {
 			Log.i(TAG, "onGetPoiResult NO_ERROR");
 			jsonPoiResultCallback(result);
-			mBaiduMap.clear();
+			if (mBaiduMap != null) {
+				mBaiduMap.clear();
+			}
 			return;
 		}
 		if (result.error == SearchResult.ERRORNO.AMBIGUOUS_KEYWORD) {
@@ -119,9 +128,7 @@ public class EBaiduMapPoiSearch implements OnGetPoiSearchResultListener,
 		if (result.error != SearchResult.ERRORNO.NO_ERROR) {
 			jsonNoResultCallback();
 		} else {
-			Toast.makeText(mContext,
-					result.getName() + ": " + result.getAddress(),
-					Toast.LENGTH_SHORT).show();
+			Toast.makeText(mContext, result.getName() + ": " + result.getAddress(), Toast.LENGTH_SHORT).show();
 		}
 	}
 
@@ -135,8 +142,7 @@ public class EBaiduMapPoiSearch implements OnGetPoiSearchResultListener,
 			super.onPoiClick(index);
 			PoiInfo poi = getPoiResult().getAllPoi().get(index);
 			// if (poi.hasCaterDetails) {
-			mPoiSearch.searchPoiDetail((new PoiDetailSearchOption())
-					.poiUid(poi.uid));
+			mPoiSearch.searchPoiDetail((new PoiDetailSearchOption()).poiUid(poi.uid));
 			// }
 			return true;
 		}
@@ -153,84 +159,85 @@ public class EBaiduMapPoiSearch implements OnGetPoiSearchResultListener,
 	}
 
 	private void jsonPoiResultCallback(PoiResult result) {
-		EBaiduMapBaseFragment activity;
-		activity = baseFragment;
-		if (activity != null) {
-			try {
-				JSONObject jsonPoi = new JSONObject();
-				jsonPoi.put(EBaiduMapUtils.MAP_PARAMS_JSON_KEY_TOTALPOINUM,
-						result.getTotalPoiNum());
-				jsonPoi.put(EBaiduMapUtils.MAP_PARAMS_JSON_KEY_TOTALPAGENUM,
-						result.getTotalPageNum());
-				jsonPoi.put(EBaiduMapUtils.MAP_PARAMS_JSON_KEY_CURRENTPAGENUM,
-						result.getCurrentPageNum());
-				jsonPoi.put(EBaiduMapUtils.MAP_PARAMS_JSON_KEY_TOTALPOINUM,
-						result.getCurrentPageCapacity());
+		try {
+			JSONObject jsonPoi = new JSONObject();
+			jsonPoi.put(EBaiduMapUtils.MAP_PARAMS_JSON_KEY_TOTALPOINUM, result.getTotalPoiNum());
+			jsonPoi.put(EBaiduMapUtils.MAP_PARAMS_JSON_KEY_TOTALPAGENUM, result.getTotalPageNum());
+			jsonPoi.put(EBaiduMapUtils.MAP_PARAMS_JSON_KEY_CURRENTPAGENUM, result.getCurrentPageNum());
+			jsonPoi.put(EBaiduMapUtils.MAP_PARAMS_JSON_KEY_TOTALPOINUM, result.getCurrentPageCapacity());
 
-				JSONArray jsonPoiInfoArray = new JSONArray();
-				for (PoiInfo poi : result.getAllPoi()) {
-					JSONObject jsonPoiInfo = new JSONObject();
-					if (poi.location != null) {
-						jsonPoiInfo.put(EBaiduMapUtils.MAP_PARAMS_JSON_KEY_LNG,
-								poi.location.longitude);
-						jsonPoiInfo.put(EBaiduMapUtils.MAP_PARAMS_JSON_KEY_LAT,
-								poi.location.latitude);
-					}
-					jsonPoiInfo.put(EBaiduMapUtils.MAP_PARAMS_JSON_KEY_NAME,
-							poi.name);
-					jsonPoiInfo.put(EBaiduMapUtils.MAP_PARAMS_JSON_KEY_UID,
-							poi.uid);
-					jsonPoiInfo.put(EBaiduMapUtils.MAP_PARAMS_JSON_KEY_ADDRESS,
-							poi.address);
-					jsonPoiInfo.put(EBaiduMapUtils.MAP_PARAMS_JSON_KEY_CITY,
-							poi.city);
-					jsonPoiInfo.put(
-							EBaiduMapUtils.MAP_PARAMS_JSON_KEY_PHONENUM,
-							poi.phoneNum);
-					jsonPoiInfo.put(
-							EBaiduMapUtils.MAP_PARAMS_JSON_KEY_POSTCODE,
-							poi.postCode);
-					String poiType = null;
-					switch (poi.type) {
-						case POINT: poiType = "0"; break;
-						case BUS_STATION: poiType = "1"; break;
-						case BUS_LINE: poiType = "2"; break;
-						case SUBWAY_STATION: poiType = "3"; break;
-						case SUBWAY_LINE: poiType = "4"; break;
-						default: break;
-				    }
-					jsonPoiInfo.put(EBaiduMapUtils.MAP_PARAMS_JSON_KEY_POITYPE,
-							poiType);
-					jsonPoiInfoArray.put(jsonPoiInfo);
+			JSONArray jsonPoiInfoArray = new JSONArray();
+			for (PoiInfo poi : result.getAllPoi()) {
+				JSONObject jsonPoiInfo = new JSONObject();
+				if (poi.location != null) {
+					jsonPoiInfo.put(EBaiduMapUtils.MAP_PARAMS_JSON_KEY_LNG, poi.location.longitude);
+					jsonPoiInfo.put(EBaiduMapUtils.MAP_PARAMS_JSON_KEY_LAT, poi.location.latitude);
 				}
-				jsonPoi.put(EBaiduMapUtils.MAP_PARAMS_JSON_KEY_POIINFO,
-						jsonPoiInfoArray);
-
-				EUExBaiduMap uexBaiduMap = activity.getUexBaseObj();
-				String js = EUExBaiduMap.SCRIPT_HEADER + "if("
-						+ EBaiduMapUtils.MAP_FUN_CB_POISEARCH_RESULT + "){"
-						+ EBaiduMapUtils.MAP_FUN_CB_POISEARCH_RESULT + "('"
-						+ jsonPoi.toString() + "');}";
-				uexBaiduMap.onCallback(js);
-			} catch (JSONException e) {
-				e.printStackTrace();
+				jsonPoiInfo.put(EBaiduMapUtils.MAP_PARAMS_JSON_KEY_NAME, poi.name);
+				jsonPoiInfo.put(EBaiduMapUtils.MAP_PARAMS_JSON_KEY_UID, poi.uid);
+				jsonPoiInfo.put(EBaiduMapUtils.MAP_PARAMS_JSON_KEY_ADDRESS, poi.address);
+				jsonPoiInfo.put(EBaiduMapUtils.MAP_PARAMS_JSON_KEY_CITY, poi.city);
+				jsonPoiInfo.put(EBaiduMapUtils.MAP_PARAMS_JSON_KEY_PHONENUM, poi.phoneNum);
+				jsonPoiInfo.put(EBaiduMapUtils.MAP_PARAMS_JSON_KEY_POSTCODE, poi.postCode);
+				String poiType = null;
+				switch (poi.type) {
+				case POINT:
+					poiType = "0";
+					break;
+				case BUS_STATION:
+					poiType = "1";
+					break;
+				case BUS_LINE:
+					poiType = "2";
+					break;
+				case SUBWAY_STATION:
+					poiType = "3";
+					break;
+				case SUBWAY_LINE:
+					poiType = "4";
+					break;
+				default:
+					break;
+				}
+				jsonPoiInfo.put(EBaiduMapUtils.MAP_PARAMS_JSON_KEY_POITYPE, poiType);
+				jsonPoiInfoArray.put(jsonPoiInfo);
 			}
+			jsonPoi.put(EBaiduMapUtils.MAP_PARAMS_JSON_KEY_POIINFO, jsonPoiInfoArray);
+
+			EUExBaiduMap uexBaiduMap = null;
+			if (baseFragment != null) {
+				uexBaiduMap = baseFragment.getUexBaseObj();
+			}
+			// change by waka
+			else if (mEBaiduMapBaseNoMapViewManager != null) {
+				uexBaiduMap = mEBaiduMapBaseNoMapViewManager.getmEUExBaiduMap();
+			} else {
+				return;
+			}
+			String js = EUExBaiduMap.SCRIPT_HEADER + "if(" + EBaiduMapUtils.MAP_FUN_CB_POISEARCH_RESULT + "){"
+					+ EBaiduMapUtils.MAP_FUN_CB_POISEARCH_RESULT + "('" + jsonPoi.toString() + "');}";
+			uexBaiduMap.onCallback(js);
+		} catch (JSONException e) {
+			e.printStackTrace();
 		}
 	}
-	
+
 	private void jsonNoResultCallback() {
-		EBaiduMapBaseFragment activity;
-		activity = baseFragment;
-		if (activity != null) {
-			EUExBaiduMap uexBaiduMap = activity.getUexBaseObj();
-			String js = EUExBaiduMap.SCRIPT_HEADER + "if("
-					+ EBaiduMapUtils.MAP_FUN_CB_POISEARCH_RESULT + "){"
-					+ EBaiduMapUtils.MAP_FUN_CB_POISEARCH_RESULT + "('" + null
-					+ "');}";
+		if (baseFragment != null) {
+			EUExBaiduMap uexBaiduMap = baseFragment.getUexBaseObj();
+			String js = EUExBaiduMap.SCRIPT_HEADER + "if(" + EBaiduMapUtils.MAP_FUN_CB_POISEARCH_RESULT + "){"
+					+ EBaiduMapUtils.MAP_FUN_CB_POISEARCH_RESULT + "('" + null + "');}";
+			uexBaiduMap.onCallback(js);
+		}
+		// change by waka
+		else if (mEBaiduMapBaseNoMapViewManager != null) {
+			EUExBaiduMap uexBaiduMap = mEBaiduMapBaseNoMapViewManager.getmEUExBaiduMap();
+			String js = EUExBaiduMap.SCRIPT_HEADER + "if(" + EBaiduMapUtils.MAP_FUN_CB_POISEARCH_RESULT + "){"
+					+ EBaiduMapUtils.MAP_FUN_CB_POISEARCH_RESULT + "('" + null + "');}";
 			uexBaiduMap.onCallback(js);
 		}
 	}
-	
+
 	public void destroy() {
 		mPoiSearch.destroy();
 		mSuggestionSearch.destroy();
